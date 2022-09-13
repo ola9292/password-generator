c4da6d4ee8ff8d96fbe9907806eff61d483cc006
Abass Olamide <51725999+ola9292@users.noreply.github.com>
2022-09-13 13:46:23 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+
+
+// let pass1El= document.getElementById('pass1-el')
+// let pass2El =document.getElementById('pass2-el')
+let pass3El = document.getElementById('my-input-1')
+let pass4El = document.getElementById('my-input-2')
+const characters = ["A","B","C","D","E","F","G","H","I","J","K",
+"L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b",
+"c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s",
+"t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7",
+ "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-",
+ "+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+
+function generatePAss(){
+    let password='';
+    for(let i=0; i <15;i++){
+        let randNum = Math.floor(Math.random()* characters.length)
+        password = password + characters[randNum]
+    
+    }
+  return password
+}
+
+function generate(){
+    // pass1El.textContent= generatePAss();
+    // pass2El.textContent = generatePAss();
+    pass3El.value = generatePAss();
+    pass4El.value = generatePAss();
+}
+//copy the first password
+function myFunction1() {
+    // Get the text field
+    var copyText = document.getElementById("my-input-1");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+    
+    
+  }
+
+  //copy the second password
+  function myFunction2() {
+    // Get the text field
+    var copyText = document.getElementById("my-input-2");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+    
+ 
+  }
